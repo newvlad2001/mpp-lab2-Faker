684802ca160074232db59963b8ce18bf644d4dba
newvlad2001 <newvlad2001@gmail.com>
2021-10-21 16:47:06 +0300

    Int and String generators moved to plugins.

---
+++ Faker.IntGenerator/IntGenerator.cs
@@ -0,0 +1,18 @@
+﻿using Faker.Library.Generators.Entity;
+using System;
+
+namespace Faker.Library.Generators.Impl.BasicGenerators
+{
+    public class IntGenerator : IGenerator
+    {
+        public object Generate(GeneratorContext context)
+        {
+            return context.Random.Next(int.MinValue, int.MaxValue);
+        }
+
+        public bool CanGenerate(Type t)
+        {
+            return typeof(int) == t;
+        }
+    }
+}
